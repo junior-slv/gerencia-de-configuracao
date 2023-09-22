6bfb82a619c313a08aa0bb7828d1698028439237
junior-slv <dirceu.junior@edu.unifil.br>
2023-09-22 19:27:27 -0300

    Atualização de produção

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Construir o projeto Next.js
 npm run build
 
-# Criar um novo diretório chamado "docs"
 mkdir docs
 
-# Copiar os arquivos da pasta ".next" para o diretório "docs"
 cp -r .next/* docs/
 
-# Crie um arquivo "404.html" (necessário para o GitHub Pages)
 echo "User-agent: *" > docs/404.html
 
-# Comprometa as alterações no GitHub
 git add .
 git commit -m "Atualização de produção"
 git push origin main
